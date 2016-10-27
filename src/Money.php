6e6f557633db49cd3f8732e0a832941584e57ba0
He Ya Dong <nuxseme@gmail.com>
2016-10-27 16:08:08 +0800

    init

---
+++ src/Money.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @link http://www.tomtop.com/
+ * @copyright Copyright (c) 2016 TOMTOP
+ * @license http://www.tomtop.com/license/
+ */
+class Money
+{
+    //金额
+    private  $amount;
+
+    //币种
+    private  $curreny;
+
+    public function __construct()
+    {
+        echo __CLASS__;
+    }
+}
